bf6bac32863ca98290d3d6099309f9155c7ea396
Christian Chirtoaca <76793483+Chirtoaca94@users.noreply.github.com>
2024-07-01 21:43:22 +0200

    Added the javascript and some styling

---
+++ script.js
@@ -0,0 +1,6 @@
+let shareBtn = document.querySelector("#share-btn");
+let share = document.querySelector(".share");
+
+shareBtn.addEventListener("click", function () {
+	share.style.visibility = "visible";
+});
